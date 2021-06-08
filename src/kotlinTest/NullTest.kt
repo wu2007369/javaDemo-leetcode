09f886b4f9ea5f15bfe71e603918d01962241d75
wuzhiming <wuzm@gongfudou.com>
2021-06-08 09:52:21 +0800

    dairy

--- src/kotlinTest/NullTest.kt
+++ src/kotlinTest/NullTest.kt
@@ -13,4 +13,9 @@ fun main(args:Array<String>) {
     println(n?:"2")
     var n3:String?=null
     println(n3?.length)
+    println(n3?.length?:"total null")
+
+    var n4:String?="与n3相对比的例子"
+    println(n4?.length?:"total null")
+    println(n4?.let { null }?:"total null,这证明，let的返回值为空，有可能导致?:后的继续执行")
 }
